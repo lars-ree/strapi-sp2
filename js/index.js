ea6832d4269651b898ac5390fdec4bd49bb28ab6
lars-ree <70200269+lars-ree@users.noreply.github.com>
2022-05-04 22:09:11 +0200

    safsa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 import { mainUrl } from "./components/api";
 
 const productsUrl = mainUrl + "/products";
+
+let strapiUrlImage = mainUrl;
+
+( async function () {
+    const container = document.querySelector(".container");
+
+    try {
+        const response = await fetch(productsUrl);
+        const json = await response.json();
+    }
+})
